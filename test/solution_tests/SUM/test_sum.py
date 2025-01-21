e9ad1471999511a8f0e26c4edda0054fc6220e0c
sbx_user1051 <sbx_user1051@169.254.71.163>
2025-01-21 05:15:54 +0000

    Tue Jan 21 05:15:54 UTC 2025

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,5 +7,3 @@ class TestSum:
     @pytest.mark.parametrize("x, y, result_sum", [(1, 2, 3), (3, 5, 8), (4, 10, 14)])
     def test_sum(self, x, y, result_sum):
         assert sum_solution.compute(x, y) == result_sum
-
-
