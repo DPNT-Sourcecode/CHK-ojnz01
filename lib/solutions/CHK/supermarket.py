174037cef5d6a5965225f719ece71c73833bf82c
sbx_user1051 <sbx_user1051@169.254.83.17>
2025-01-21 10:15:53 +0000

    Tue Jan 21 10:15:53 UTC 2025

--- lib/solutions/CHK/supermarket.py
+++ lib/solutions/CHK/supermarket.py
@@ -5,23 +5,28 @@ class Calculator:
 
     def __init__(self, offer_dict: Dict) -> None:
         self.offer_dict = offer_dict
+        self.free_item = {}
 
     def calculate_final_offer(self, product: str, unit: int) -> int:
         final_offer = 0
         price_per_unit = 0
         offer_unit = None
         offer_price = None
-        free_item = None
+        offer_free_item = None
 
         product_info = self.offer_dict.get(product)
         if product_info:
             product_offer = product_info.get("offer")
             if product_offer:
                 for offer_item in product_offer:
-                    offer_unit = product_offer["unit"]
+                    offer_unit_temp = product_offer["unit"]
 
-                    offer_price = product_offer.get("final_price", None)
-                    free_item = product_offer.get("free_item", None)
+                    if offer_unit and (unit >= offer_unit) and unit < offer_unit_temp:
+                        break
+                    else:
+                        offer_unit = offer_unit_temp
+                        offer_price = product_offer.get("final_price", None)
+                        offer_free_item = product_offer.get("free_item", None)
 
             price_per_unit = product_info["price"]
         else:
@@ -34,9 +39,13 @@ class Calculator:
             if remain_unit >= 1:
                 final_offer += price_per_unit * remain_unit
         elif offer_unit and free_item:
-            free_item_unit = unit // offer_unit
+            self.free_item[offer_free_item] = unit // offer_unit
             final_offer = price_per_unit * unit
         else:
             final_offer = price_per_unit * unit
 
         return final_offer
+
+    def getfree_items(self):
+        return self.free_item
+
--- test/solution_tests/CHK/test_checkout_r2.py
+++ test/solution_tests/CHK/test_checkout_r2.py
@@ -1,31 +1,36 @@
 import pytest
+from solutions.CHK.checkout_r2_soloution import checkout_chk_r2
 
 
 OFFER_DISCOUNT_CHK_R1 = {
-    "A": {"price": 50, "offer": [
-        {"unit": 3, "final_price": 130},
-        {"unit": 5, "final_price": 200}
-        ]},
-    "B": {"price": 30, "offer": [
-        {"unit": 2, "final_price": 45}
-        ]},
+    "A": {
+        "price": 50,
+        "offer": [{"unit": 3, "final_price": 130}, {"unit": 5, "final_price": 200}],
+    },
+    "B": {"price": 30, "offer": [{"unit": 2, "final_price": 45}]},
     "C": {"price": 20},
     "D": {"price": 15},
-    "E": {"price": 40, "offer": {"unit": 2, "free_item": "B"}}
+    "E": {"price": 40, "offer": {"unit": 2, "free_item": "B"}},
 }
 
 
-@pytest.mark.parametrize("skus, result", [
-    ("A, A, A, B, C", 180),
-    ("", 0),
-    ("test", -1),
-    ("A, A, A, B, B, B, C, D", 240),
-    ("A, B, C, D", 115),
-    ("-", -1),
-    ("BABDDCAC", 215),
-    ("B", 30),
-    ("AA", 100),
-    ("AxA", -1),
-    ({"test"}, -1),
-    ("AABEE", 140)
-]) 
+@pytest.mark.parametrize(
+    "skus, result",
+    [
+        ("A, A, A, B, C", 180),
+        ("", 0),
+        ("test", -1),
+        ("A, A, A, B, B, B, C, D", 240),
+        ("A, B, C, D", 115),
+        ("-", -1),
+        ("BABDDCAC", 215),
+        ("B", 30),
+        ("AA", 100),
+        ("AxA", -1),
+        ({"test"}, -1),
+        ("AABEE", 140),
+    ],
+)
+def test_checkout_r2(skus, result):
+    checkout_test = checkout_chk_r2(skus)
+    assert checkout_test == result
