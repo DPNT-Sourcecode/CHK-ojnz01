4da4c5d2abfe14bf27c9e34f974756d4531a0bc9
sbx_user1051 <sbx_user1051@169.254.71.163>
2025-01-21 05:12:46 +0000

    Tue Jan 21 05:12:46 UTC 2025

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,7 +4,8 @@ from solutions.SUM import sum_solution
 
 class TestSum:
 
-    @pytest.mark.parametrize("x, y, result_sum", [(1, 2, 3), ()])
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+    @pytest.mark.parametrize("x, y, result_sum", [(1, 2, 3), (3, 5, 8), (4, 10, 14)])
+    def test_sum(self, x, y, result_sum):
+        assert sum_solution.compute(x, y) == result_sum
+
 
