f00a24609b020173e92cd3358003c95834bc4140
sbx_user1051 <sbx_user1051@169.254.83.17>
2025-01-21 06:36:31 +0000

    Tue Jan 21 06:36:31 UTC 2025

--- lib/solutions/CHK/supermarket.py
+++ lib/solutions/CHK/supermarket.py
@@ -9,11 +9,16 @@ class Calculator:
     def calculate_final_offer(self, product: str, unit: int) -> int:
         final_result = 0
         offer_info = {}
+        offer_unit = None
+        offer_price = None
+        price_per_unit = 0
 
         for product in self.offer_dict:
             if "offer" in product:
                 offer_unit = product["offer"]["unit"]
-                offer_price = product["offer"]["unit"]
+                offer_price = product["offer"]["final_price"]
+            price_per_unit = product["price"]
 
         return final_result
+
 
