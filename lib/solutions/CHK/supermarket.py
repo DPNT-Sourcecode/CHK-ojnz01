9fd51b518cb031bdf248cbdaa242a8086a14c513
sbx_user1051 <sbx_user1051@169.254.83.17>
2025-01-21 06:27:31 +0000

    Tue Jan 21 06:27:31 UTC 2025

---
+++ lib/solutions/CHK/supermarket.py
@@ -0,0 +1,11 @@
+
+from typing import Dict
+
+
+class Calculator:
+    
+    def __init__(self, offer_dict: Dict) -> None:
+        self.offer_dict = offer_dict 
+    
+    def calculate_final_offer(unit: str) -> Int:
+        
